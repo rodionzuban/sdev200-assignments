121ba386617b4e97fb4f47a492be9d5ff2dcd95d
rodion zuban <rodia.zuban@gmail.com>
2026-02-12 23:12:32 -0500

    Removed README

--- module5/Exercise14_01/src/App.java
+++ module5/Exercise14_01/src/App.java
@@ -11,6 +11,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+        // import flag images and set proper aspect ratios
+
         Image flag1 = new Image("file:Images/flag1.gif");
         ImageView flag1ImageView = new ImageView(flag1);
         flag1ImageView.setFitWidth(400);
@@ -31,15 +33,21 @@ public class App extends Application {
         flag3ImageView.setFitWidth(400);
         flag3ImageView.setFitHeight(300);
 
+        // create new grid pane
         GridPane gridPane = new GridPane();
 
+        // add padding
         gridPane.setPadding(new Insets(5, 5, 5, 5));
 
+        // gaps between images
         gridPane.setVgap(10);
         gridPane.setHgap(10);
 
         gridPane.setAlignment(Pos.CENTER);
 
+        // add images in correct position
+        // NOTE: Germany's flag as shown in textbook is replaced by UK flag provided by
+        // instructor
         gridPane.add(flag2ImageView, 0, 0);
         gridPane.add(flag4ImageView, 1, 0);
         gridPane.add(flag3ImageView, 0, 1);
